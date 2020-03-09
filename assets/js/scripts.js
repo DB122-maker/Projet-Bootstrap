84ada11002b000be5d177c87db71e57664427d36
DB <selemi8334@hxqmail.com>
2020-03-09 05:18:37 +0100

    db122

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 function scroll_to(clicked_link, nav_height) {
-  var element_class = clicked_link.attr("href").replace("#", ".");
-  var scroll_to = 0;
+  let element_class = clicked_link.attr("href").replace("#", ".");
+  let scroll_to = 0;
   if (element_class != ".top-content") {
     element_class += "-container";
     scroll_to = $(element_class).offset().top - nav_height;
